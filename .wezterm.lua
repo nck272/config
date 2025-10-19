c51997bf684e08e43576cc7066bad43ae3eac06f
nck272 <nck272.dev@gmail.com>
2025-10-19 23:12:25 +0700

    first commit

---
+++ .wezterm.lua
@@ -0,0 +1,56 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.default_domain = 'WSL:Ubuntu'
+config.default_cursor_style = 'SteadyBar'
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'Batman'
+
+-- Set background to same color as neovim
+config.colors = {}
+config.colors.background = '#111111'
+
+config.font = wezterm.font_with_fallback {
+  'IosevkaTerm Nerd Font Mono',
+  'nonicons',
+}
+
+-- default is true, has more "native" look
+config.use_fancy_tab_bar = false
+
+-- I don't like putting anything at the ege if I can help it.
+config.enable_scroll_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.tab_bar_at_bottom = true
+config.freetype_load_target = "HorizontalLcd"
+
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+}
+
+config.color_scheme = '3024 (dark) (terminal.sexy)'
+
+-- and finally, return the configuration to wezterm
+return config
